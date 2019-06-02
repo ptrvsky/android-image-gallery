0a926fed3cd8dd5cbf9989dd1fb1dd0ee4ccb199
Peterovsky <Peterovsky7@gmail.com>
2019-06-02 15:56:20 +0200

    Add image sliding animation

--- app/src/main/java/io/github/ptrvsky/imagegallery/ImageSlideFragment.java
+++ app/src/main/java/io/github/ptrvsky/imagegallery/ImageSlideFragment.java
@@ -36,6 +36,7 @@ public class ImageSlideFragment extends Fragment {
         FragmentManager fragmentManager = getChildFragmentManager();
         mPager = (ViewPager) view.findViewById(R.id.pager);
         pagerAdapter = new ScreenSlidePagerAdapter(fragmentManager);
+        mPager.setPageTransformer(true, new DepthPageTransformer());
         mPager.setAdapter(pagerAdapter);
         mPager.setCurrentItem(getArguments().getInt("viewPagerPosition"));
 
@@ -73,5 +74,35 @@ public class ImageSlideFragment extends Fragment {
         }
     }
 
+    public class DepthPageTransformer implements ViewPager.PageTransformer {
+        private static final float MIN_SCALE = 0.75f;
+
+        public void transformPage(View view, float position) {
+            int pageWidth = view.getWidth();
+
+            if (position < -1) {
+                view.setAlpha(0f);
+
+            } else if (position <= 0) {
+                view.setAlpha(1f);
+                view.setTranslationX(0f);
+                view.setScaleX(1f);
+                view.setScaleY(1f);
+
+            } else if (position <= 1) {
+                view.setAlpha(1 - position);
+
+                view.setTranslationX(pageWidth * -position);
+
+                float scaleFactor = MIN_SCALE
+                        + (1 - MIN_SCALE) * (1 - Math.abs(position));
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+            } else {
+                view.setAlpha(0f);
+            }
+        }
+    }
 
 }
